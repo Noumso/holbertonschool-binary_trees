08bce5688e2b113f27995e56913975315f6ef39b
Noumso <noumane.82300@gmail.com>
2024-12-09 17:30:49 +0000

    first commit

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,1 +1,9 @@
-#include binary_trees.h
+#include "binary_trees.h"
+/**
+ * binary_tree_insert_left - inserts a node as the left-child of another node
+ *
+ * @parent: parent node
+ * @value: data
+ * Return: binary_tree_t*
+ */
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
