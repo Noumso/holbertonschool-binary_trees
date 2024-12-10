1218cb456c8f05893ebefd829cbd61dcb3d3a63d
root <Noumso>
2024-12-10 19:25:56 +0000

    add func to count node

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_node - measures the node of a binary tree
+ *
+ * @tree: root of binary tree
+ * Return: height
+ */
+size_t binary_tree_nodes(const bonary_tree_t *tree)
+{
+	size_t nodes = 0;
+
+	if (tree == NULL)
+		return (0);
+
+	nodes += (tree->left || tree->right ? 1 : 0);
+	node += binary_tree_nodes(tree->left);
+	nodes += binary_tree_nodes(tree->right);
+
+	return (nodes);
+}
